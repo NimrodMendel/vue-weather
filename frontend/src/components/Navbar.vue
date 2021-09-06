55d9a29548214007a40b5103233f9c4b37ae5c50
Nimrod Mendel <mendel.nimrod@gmail.com>
2021-09-06 12:17:47 +0300

    Add frontend

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <w-toolbar shadow class="py3">
+    <w-icon color="light-blue-dark3" size="2.5em" class="mr3">wi-wave</w-icon>
+    <div class="title2">Vue Weather App</div>
+  </w-toolbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
